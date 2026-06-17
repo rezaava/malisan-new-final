c7ba531bfc7fdb934041125060bf2a14f41e7d3e
Arshia-AJ-10 <arshiaaj132@gmail.com>
2026-06-16 23:09:10 -0700

    befor vendor and env A

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TestController;
 
-
-
-
     Route::get('/', [TestController::class, 'index'])->name('index');
     Route::get('/courses', [TestController::class, 'courses'])->name('courses');
-
