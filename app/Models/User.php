604b678152839bbcf6335bd243dbf3b90c2ef1ba
Arshia-AJ-10 <arshiaaj132@gmail.com>
2026-06-17 07:37:19 -0700

    env and vendor

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,53 +2,147 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Laratrust\Contracts\LaratrustUser;
-use Laratrust\Traits\HasRolesAndPermissions;
-use App\Models\Cart;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable implements LaratrustUser
+class User extends Model
 {
+    use HasFactory;
 
-    use HasRolesAndPermissions;
-
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'users';
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'role', 'name', 'family', 'gender', 'email', 'national', 'shenasname',
+        'personal', 'birthdate', 'city_birth', 'city', 'address', 'postal',
+        'tel', 'mobile', 'sms', 'active', 'tour', 'tel_work', 'uni_email',
+        'web', 'scholar', 'social', 'degree', 'field', 'trend', 'trend_en',
+        'research', 'image', 'shaba', 'turn', 'password', 'aneto_token'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    // روابط
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'course_user', 'user_id', 'course_id');
+    }
 
-    public function cart(){
-        return $this->hasOne(Cart::class);
+    public function amalis()
+    {
+        return $this->hasMany(Amali::class, 'user_id');
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class, 'user_id');
+    }
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class, 'student_id');
+    }
+
+    public function coworkers()
+    {
+        return $this->hasMany(Coworker::class, 'user_id');
+    }
+
+    public function discussions()
+    {
+        return $this->hasMany(Discussion::class, 'user_id');
+    }
+
+    public function exercises()
+    {
+        return $this->hasMany(Exercise::class, 'user_id');
+    }
+
+    public function exerciseAnswers()
+    {
+        return $this->hasMany(ExerciseAnswer::class, 'user_id');
+    }
+
+    public function fcms()
+    {
+        return $this->hasMany(Fcm::class, 'user_id');
+    }
+
+    public function incomes()
+    {
+        return $this->hasMany(Income::class, 'user_id');
+    }
+
+    public function konkorqs()
+    {
+        return $this->hasMany(Konkorq::class, 'user_id');
+    }
+
+    public function leitners()
+    {
+        return $this->hasMany(Leitner::class, 'user_id');
+    }
+
+    public function optionUsers()
+    {
+        return $this->hasMany(OptionUser::class, 'user_id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class, 'user_id');
+    }
+
+    public function quizzes()
+    {
+        return $this->hasMany(Quiz::class, 'user_id');
+    }
+
+    public function scores()
+    {
+        return $this->hasMany(Score::class, 'user_id');
+    }
+
+    public function shops()
+    {
+        return $this->hasMany(Shop::class, 'user_id');
+    }
+
+    public function studentAdjectives()
+    {
+        return $this->hasMany(StudentAdjective::class, 'student_id');
+    }
+
+    public function studentAdjectivesTeacher()
+    {
+        return $this->hasMany(StudentAdjective::class, 'teacher_id');
+    }
+
+    public function studentEvents()
+    {
+        return $this->hasMany(StudentEvent::class, 'student_id');
+    }
+
+    public function studentEventsTeacher()
+    {
+        return $this->hasMany(StudentEvent::class, 'teacher_id');
+    }
+
+    public function surveys()
+    {
+        return $this->hasMany(Survey::class, 'user_id');
+    }
+
+    public function touradmins()
+    {
+        return $this->hasMany(Touradmin::class, 'user_id');
+    }
+
+    public function tourusers()
+    {
+        return $this->hasMany(Touruser::class, 'user_id');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
     Route::get('/', [TestController::class, 'index'])->name('index');
-<<<<<<< HEAD
-    Route::get('/courses', [TestController::class, 'courses'])->name('courses');
-=======
     Route::get('/courses', [TestController::class, 'courses'])->name('courses');
     Route::get('/publics', [TestController::class, 'publics'])->name('publics');
     Route::get('/exams', [TestController::class, 'exams'])->name('exams');
@@ -15,11 +12,3 @@ use App\Http\Controllers\TestController;
     Route::get('/content', [TestController::class, 'content'])->name('content');
     Route::get('/create-quiz', [TestController::class, 'createQuiz'])->name('createQuiz');
     Route::get('/quizzes', [TestController::class, 'quizzes'])->name('quizzes');
-
-
-
-    
-
-
-
->>>>>>> dbfb08111916876ac86d05ba89c8dd583a86cabc
